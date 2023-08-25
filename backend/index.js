2b90579ffe664007279a0ce88cb16e694d275cfa
Aamir041 <aamirsaudagar34@gmail.com>
2023-08-25 22:13:46 +0530

    I AM LONELY

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express'); // express js ki saare functionality is variable me laa raha hu
+
+const port = 8000; // server will run on this port
+
+const app = express();
+
+// API of GET type : / return text "Hello World" on screen
+app.get("/", (req,res) => {
+    res.send("HELLO WORLD");
+})
+
+app.listen(port,() => {
+    console.log("App is running on this port.");
+})
