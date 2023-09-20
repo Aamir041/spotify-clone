51030c6cd620d7cf0cefbbda09259deea674714c
Aamir041 <aamirsaudagar34@gmail.com>
2023-09-21 00:01:27 +0530

    added eye option

--- frontend/src/components/Shared/PasswordInput.jsx
+++ frontend/src/components/Shared/PasswordInput.jsx
@@ -1,13 +1,28 @@
-const PasswordInput = ({label,placeholder, styleInput}) => {
+import { useState } from "react";
+import { Icon } from "@iconify/react";
+
+const PasswordInput = ({label,placeholder, styleInput,value,setValue}) => {
+
+    const [eyeOpen,setEyeOpen] = useState(false);
+
     return (
-        <div className={`passwordInputDiv flex flex-col space-y-2 w-full ${styleInput}`}>
-            <label className="font-semibold" htmlFor="inp">{label}</label>
-            <input
-                type="password"
-                placeholder={placeholder}
-                className="p-2 border border-gray-400 border-solid rounded placeholder-gray-500"
-                id="inp"
-            />
+        <div className={`passwordInputDiv flex w-full ${styleInput} items-end space-x-3`}>
+            <div className="flex flex-col w-full space-y-2">
+
+                <label className="font-semibold" htmlFor="inp">{label}</label>
+                <input
+                    type={eyeOpen ? "text" : "password"}
+                    placeholder={placeholder}
+                    className="p-2 border border-gray-400 border-solid rounded placeholder-gray-500"
+                    id="inp"
+                    onChange={(e) => setValue(e.target.value)}
+                />
+            </div>
+            <button onClick={() => setEyeOpen(!eyeOpen)} className=" text-4xl">
+                {
+                    eyeOpen ? <Icon icon="quill:eye-closed" /> : <Icon icon="iconamoon:eye-thin" />
+                }
+            </button>
         </div>
     )
 }
--- frontend/src/components/Shared/TextInput.jsx
+++ frontend/src/components/Shared/TextInput.jsx
@@ -1,12 +1,12 @@
-const TextInput = ({label,placeholder,styleInput}) => {
+const TextInput = ({label,placeholder,styleInput, value, setValue}) => {
     return (
         <div className={`inputTextDiv flex flex-col space-y-2 w-full ${styleInput}`}>
-            <label className="font-semibold" htmlFor="inp">{label}</label>
+            <label className="font-semibold">{label}</label>
             <input
                 type="text"
                 placeholder={placeholder}
                 className="p-2 border border-gray-400 border-solid rounded placeholder-gray-500"
-                id="inp"
+                onChange={(e)=>setValue(e.target.value)}
             />
         </div>
     )
