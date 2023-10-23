fb0f0fb08e06c8ddc8b9b405e0cf02718a644c37
Aamir041 <aamirsaudagar34@gmail.com>
2023-10-23 10:42:30 +0530

    song context added not yet used thod

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,25 +7,28 @@ import LoggedInHomeComponent from "./routes/LoggedInHomeComponent";
 import { useCookies } from "react-cookie";
 import UploadSongs from "./routes/UploadSong";
 import MyMusic from "./routes/MyMusic";
+import songContext from "./context/songContext";
+import { useState } from "react";
 
 function App() {
 
   const [cookie,setCookie] = useCookies(["token"]);
+  const [currentSong,setCurrentSong] = useState();
   
   return (
     <div className="app w-screen h-screen font-poppins">
         <BrowserRouter>
             {cookie?.token ? (
               // If Cookie Exist go to these routes LOGGED IN ROUTES
-              <Routes>
-                <Route path="/" element={<h1 className="bg-red-500">Hello</h1>} />
-                <Route path="/home" element={<LoggedInHomeComponent />} />
-                <Route path="/uploadsong" element={<UploadSongs/>}/>
-                <Route path="/mymusic" element={<MyMusic/>}/>
-
-                {/* For any route that does not exist in above routes use path as "*" */}
-                <Route path="*" element={<Navigate to="/home" />} />
-              </Routes>
+                <songContext.Provider value={{currentSong,setCurrentSong}}>
+                  <Routes>
+                      <Route path="/home" element={<LoggedInHomeComponent />} />
+                      <Route path="/uploadsong" element={<UploadSongs/>}/>
+                      <Route path="/mymusic" element={<MyMusic/>}/>
+                      {/* For any route that does not exist in above routes use path as "*" */}
+                      <Route path="*" element={<Navigate to="/home" />} />
+                  </Routes>
+                </songContext.Provider>
             ) : (
               // If Cookie does not Exist go to these routes LOGGED OUT ROUTES
               <Routes>
